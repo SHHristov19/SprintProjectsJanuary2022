801025b8b19f6ab55040e741d332d6f5a40474e5
DKKolev19 <63718751+DKKolev19@users.noreply.github.com>
2022-02-12 20:24:58 +0200

    Add comment in FrontEnd.h

--- ConsoleApplication/Project2022January/FrontEnd.h
+++ ConsoleApplication/Project2022January/FrontEnd.h
@@ -2,56 +2,83 @@
 #include <iostream>
 using namespace std;
 
+//Sets on Sign In and prints a part of the menu
 void activeSignIn();
 
+//Prints Sign In on the start menu and prints a part of the menu
 void writeSignIn();
 
+//Sets arrow on See Story
 void activeSeeStory();
 
+//Prints See Story on the start menu
 void writeSeeStory();
 
+//Prints Add Story on the start menu
 void writeAddStory();
 
+//Sets arrow on Add Story
 void activeAddStory();
 
+//Prints Search Story on the start menu
 void writeSearch();
 
+//Sets arrow on Search Story
 void activeSearch();
 
+//Print How it works on the start menu
 void writeHowItWorks();
 
+//Sets arrow on How it works
 void activeHowItWorks();
 
+//Prints About us on the start menu
 void writeAboutUs();
 
+//Sets arrow on About us
 void activeAboutUs();
 
+//Prints Exit on the start menu
 void writeExit();
 
+//Sets arrow on Exit
 void activeExit();
 
+//Prints "Back" on a selected option allowing you to return
 void writeBackWhite();
 
+//Prints ASCII Art title and information about the team
 void printAboutUs();
 
+//Prints ASCII Art title of Add Story in yellow
 void printTitleHowItWorks();
 
+//Prints information about the program and how it must be used
 void printHowItWorks();
 
+//Prints ASCII Art title of Add Story in yellow
 void printTitleAddStory();
 
+//Prints ASCII Art title of See story in yellow
 void printTitleSeeStory();
 
+//Moves arrow on Sign in
 void printActiveSignIn();
 
+//Moves arrow on See Story
 void printActiveSeeStory();
 
+//Moves arrow on Add Story
 void printActiveAddStory();
 
+//Moves arrow on Search Story
 void printActiveSearch();
 
+//Moves arrow on How it works
 void printActiveHowItWorks();
 
+//Moves arrow on About us
 void printActiveAboutUs();
 
+//Moves arrow on Exit
 void printActiveExit();
